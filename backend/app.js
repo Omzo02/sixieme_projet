d845d8f268cc84bce4106caf4ac0a9377c184d7c
Omzo02 <odiop75@gmail.com>
2025-03-23 18:32:05 +0100

    Installation MongoDB et Application express

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb+srv://sixieme_projet:***REMOVED***@cluster2.02aqv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 // Middleware pour gérer les JSON
